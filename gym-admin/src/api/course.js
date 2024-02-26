3c0c52582c2c56fdf12c85fe89ef76b1b0624011
通晓宇宙 <2581011320@qq.com>
2024-02-26 11:17:37 +0800

    课程管理1

--- gym-admin/src/api/course.js
+++ gym-admin/src/api/course.js
@@ -1,1 +1,142 @@
 import request from "@/utils/system/request";
+
+export function saveProject(data) {
+  return request({
+    url: "/api/course/project",
+    method: "post",
+    data
+  });
+}
+
+export function removeProject(id) {
+  return request({
+    url: `/api/course/project/${id}`,
+    method: "delete"
+  });
+}
+
+export function removeProjects(data) {
+  return request({
+    url: "/api/course/projects",
+    method: "delete",
+    data
+  });
+}
+
+export function updateProject(data) {
+  return request({
+    url: "/api/course/project",
+    method: "put",
+    data
+  });
+}
+
+export function getProject(id) {
+  return request({
+    url: `/api/course/project/${id}`,
+    method: "get"
+  });
+}
+
+export function listProjects(data, current, size) {
+  return request({
+    url: "/api/course/projects",
+    method: "post",
+    params: { current, size },
+    data
+  });
+}
+
+export function saveCourse(data) {
+  return request({
+    url: "/api/course/course",
+    method: "post",
+    data
+  });
+}
+
+export function removeCourse(id) {
+  return request({
+    url: `/api/course/course/${id}`,
+    method: "delete"
+  });
+}
+
+export function removeCourses(data) {
+  return request({
+    url: "/api/course/courses",
+    method: "delete",
+    data
+  });
+}
+
+export function updateCourse(data) {
+  return request({
+    url: "/api/course/course",
+    method: "put",
+    data
+  });
+}
+
+export function getCourse(id) {
+  return request({
+    url: `/api/course/course/${id}`,
+    method: "get"
+  });
+}
+
+export function listCourses(data, current, size) {
+  return request({
+    url: "/api/course/courses",
+    method: "post",
+    params: { current, size },
+    data
+  });
+}
+
+export function saveCoach(data) {
+  return request({
+    url: "/api/course/coach",
+    method: "post",
+    data
+  });
+}
+
+export function removeCoach(id) {
+  return request({
+    url: `/api/course/coach/${id}`,
+    method: "delete"
+  });
+}
+
+export function removeCoaches(data) {
+  return request({
+    url: "/api/course/coaches",
+    method: "delete",
+    data
+  });
+}
+
+export function updateCoach(data) {
+  return request({
+    url: "/api/course/coach",
+    method: "put",
+    data
+  });
+}
+
+export function getCoach(id) {
+  return request({
+    url: `/api/course/coach/${id}`,
+    method: "get"
+  });
+}
+
+export function listCoaches(data, current, size) {
+  return request({
+    url: "/api/course/coaches",
+    method: "post",
+    params: { current, size },
+    data
+  });
+}
--- gym-admin/src/views/main/course/project/index.vue
+++ gym-admin/src/views/main/course/project/index.vue
@@ -1,1 +1,185 @@
-<template>训练项目</template>
+<template>
+  <div class="layout-container">
+    <!-- 头部 -->
+    <div class="layout-container-form flex space-between">
+      <div class="layout-container-form-handle">
+        <el-button type="primary" :icon="Plus" @click="openAddDialog">添加</el-button>
+        <el-popconfirm
+          title="确定删除选中的数据吗？"
+          @confirm="deleteChoose"
+          confirm-button-text="确定"
+          cancel-button-text="取消"
+        >
+          <template #reference>
+            <el-button type="danger" :icon="Delete" :disabled="chooseData.length === 0">删除选中</el-button>
+          </template>
+        </el-popconfirm>
+      </div>
+      <div class="layout-container-form-search">
+        <el-input v-model="query.name" placeholder="项目名称"></el-input>
+        <el-input v-model="query.description" placeholder="项目描述"></el-input>
+        <el-button type="primary" :icon="Search" class="search-btn" @click="getTableData">搜索</el-button>
+      </div>
+    </div>
+
+    <!-- 表格 -->
+    <div class="layout-container-table">
+      <Table
+        ref="table"
+        v-model:page="page"
+        v-loading="loading"
+        row-key="id"
+        :showSelection="true"
+        :data="tableData"
+        @getTableData="getTableData"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column prop="name" label="项目名称"></el-table-column>
+        <el-table-column prop="description" label="项目描述" :show-overflow-tooltip="true"></el-table-column>
+
+        <el-table-column label="创建时间" prop="createTime" :show-overflow-tooltip="true"></el-table-column>
+        <el-table-column label="更新时间" prop="updateTime" :show-overflow-tooltip="true"></el-table-column>
+
+        <el-table-column label="操作">
+          <template v-slot="scope">
+            <el-button type="primary" @click="openEditDialog(scope.row)">修改</el-button>
+            <el-popconfirm
+              title="确定删除吗？"
+              @confirm="deleteOne(scope.row)"
+              confirm-button-text="确定"
+              cancel-button-text="取消"
+            >
+              <template #reference>
+                <el-button type="danger">删除</el-button>
+              </template>
+            </el-popconfirm>
+          </template>
+        </el-table-column>
+      </Table>
+    </div>
+
+    <!-- 弹窗 -->
+    <Layer :layer="layer" @confirm="submit">
+      <el-form ref="form" :model="formModel" label-width="100px">
+        <el-form-item label="项目名称" prop="name">
+          <el-input v-model="formModel.name" placeholder="请输入项目名称"></el-input>
+        </el-form-item>
+        <el-form-item label="项目描述" prop="description">
+          <el-input v-model="formModel.description" placeholder="请输入项目描述"></el-input>
+        </el-form-item>
+      </el-form>
+    </Layer>
+  </div>
+</template>
+
+<script setup>
+import { saveProject, removeProject, removeProjects, updateProject, getProject, listProjects } from "@/api/course";
+import Layer from "@/components/layer/index.vue";
+import Table from "@/components/table/index.vue";
+import { Delete, Plus, Search } from "@element-plus/icons";
+import { ElMessage } from "element-plus";
+import { onBeforeMount, reactive, ref } from "vue";
+
+// 搜索相关
+const query = reactive({
+  name: "",
+  description: ""
+});
+
+// 弹窗控制器
+const layer = reactive({
+  show: false,
+  title: "新增",
+  showButton: true
+});
+
+// 弹窗表单数据
+const formModel = ref({
+  name: "",
+  description: ""
+});
+
+// 分页参数, 供table使用
+const page = reactive({
+  index: 1,
+  size: 20,
+  total: 0
+});
+
+const loading = ref(true);
+const tableData = ref([]);
+const chooseData = ref([]);
+const handleSelectionChange = val => {
+  chooseData.value = val;
+};
+
+// 获取表格数据
+function getTableData() {
+  listProjects(query, page.index, page.size).then(res => {
+    tableData.value = res.records;
+    // page.index = res.current
+    // page.size = res.size
+    page.total = res.total;
+    loading.value = false;
+  });
+}
+
+onBeforeMount(() => {
+  getTableData();
+});
+
+// 删除选中数据
+function deleteChoose() {
+  const ids = chooseData.value.map(item => item.id);
+  removeProjects(ids).then(() => {
+    ElMessage.success("删除成功");
+    getTableData();
+  });
+}
+
+// 删除单条数据
+function deleteOne(row) {
+  removeProject(row.id).then(() => {
+    ElMessage.success("删除成功");
+    getTableData();
+  });
+}
+
+// 新增/编辑弹窗
+function openAddDialog() {
+  formModel.value = {
+    name: "",
+    description: ""
+  };
+
+  layer.title = "新增记录";
+  layer.show = true;
+  layer.showButton = true;
+}
+
+function openEditDialog(row) {
+  getProject(row.id).then(res => {
+    formModel.value = JSON.parse(JSON.stringify(res));
+    layer.title = "编辑记录";
+    layer.show = true;
+    layer.showButton = true;
+  });
+}
+
+// 提交新增/修改
+function submit() {
+  if (formModel.value.id) {
+    updateProject(formModel.value).then(() => {
+      ElMessage.success("修改成功");
+      layer.show = false;
+      getTableData();
+    });
+  } else {
+    saveProject(formModel.value).then(() => {
+      ElMessage.success("新增成功");
+      layer.show = false;
+      getTableData();
+    });
+  }
+}
+</script>
