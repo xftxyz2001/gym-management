31c8ccf41797ef4552ae040e84538184e3dbd1c4
通晓宇宙 <2581011320@qq.com>
2024-03-01 10:57:01 +0800

    更新时间

---
+++ gym-server/src/main/java/com/xftxyz/gymadmin/config/DateConfig.java
@@ -0,0 +1,22 @@
+package com.xftxyz.gymadmin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Date;
+
+@Configuration
+public class DateConfig implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createTime", new Date(), metaObject);
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+}
--- gym-server/src/main/java/com/xftxyz/gymadmin/domain/Member.java
+++ gym-server/src/main/java/com/xftxyz/gymadmin/domain/Member.java
@@ -1,9 +1,6 @@
 package com.xftxyz.gymadmin.domain;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
@@ -83,13 +80,13 @@ public class Member implements Serializable {
     /**
      * 创建时间
      */
-    @TableField(value = "create_time")
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
     private Date createTime;
 
     /**
      * 更新时间
      */
-    @TableField(value = "update_time")
+    @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
 
     /**
